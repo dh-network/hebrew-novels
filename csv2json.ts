e3b88c0fcea0277355b8f13c9852b05eea992f12
Carsten Milling <cmil@hashtable.de>
2022-09-17 12:23:05 +0200

    Generate preliminary IDs

--- csv2json.ts
+++ csv2json.ts
@@ -1,5 +1,6 @@
 import {readFileSync, writeFileSync} from 'fs';
 import { parse } from 'csv-parse/sync';
+import { v4 as uuidv4 } from 'uuid';
 import { novelFields, readingFields } from './src/fields';
 
 const novelsCsvPath = './austere-list.csv';
@@ -10,6 +11,7 @@ const readingsJsonPath = './public/readings.json';
 const novelsCsv = readFileSync(novelsCsvPath, 'utf8');
 const novels = parse(novelsCsv, {columns: true, trim: false}).map((r: any) => {
   const book: any = {};
+  book.id = uuidv4();
   novelFields.forEach(([column, name]) => {
     book[name] = r[column];
   });
@@ -20,6 +22,7 @@ writeFileSync(novelsJsonPath, JSON.stringify(novels, undefined, 2));
 const readingsCsv = readFileSync(readingsCsvPath, 'utf8');
 const readings = parse(readingsCsv, {columns: true, trim: false}).map((r: any) => {
   const reading: any = {};
+  reading.id = uuidv4();
   readingFields.forEach(([column, name]) => {
     reading[name] = r[column];
   });
