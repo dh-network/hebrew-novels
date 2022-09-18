564c2834987a3fbee319d5c11f26ecc050622206
Carsten Milling <cmil@hashtable.de>
2022-09-18 10:40:49 +0200

    Replace lists with bootstrap tables

--- src/Novels.tsx
+++ src/Novels.tsx
@@ -1,10 +1,48 @@
 import {useContext} from 'react';
 import {Helmet} from 'react-helmet';
 import {Link} from 'react-router-dom';
+import BootstrapTable from 'react-bootstrap-table-next';
+import ToolkitProvider from 'react-bootstrap-table2-toolkit';
+// import ToolkitProvider, {Search} from 'react-bootstrap-table2-toolkit';
 import {NovelsContext} from './context';
+import {Novel} from './types';
+
+function formatTitle (_: string, novel: Novel) {
+  const {title, id} = novel;
+  return (
+    <Link className="text-lg" to={id}>{title}</Link>
+  );
+}
 
 export default function Novels () {
   const {novels} = useContext(NovelsContext);
+
+  const data = novels.map((n: Novel) => {
+    return {...n};
+  });
+
+  const columns = [{
+    dataField: 'title',
+    text: 'Title',
+    formatter: formatTitle,
+    sort: true,
+  }, {
+    dataField: 'author',
+    text: 'Author',
+    sort: true,
+  }, {
+    dataField: 'publisher',
+    text: 'Publisher',
+    sort: true,
+  }, {
+    dataField: 'publicationYear',
+    text: 'Year',
+    sort: true,
+  }, {
+    dataField: 'numPages',
+    text: 'Pages',
+    sort: true,
+  }];
 
   return (
     <div>
@@ -13,11 +51,17 @@ export default function Novels () {
       </Helmet>
       <section>
         <h1>Novels</h1>
-        <ol dir="rtl">
-          {novels.map(novel => (
-            <li key={novel.id}><Link to={novel.id}>{novel.title}</Link></li>
-          ))}
-      </ol>
+
+        <BootstrapTable
+          bootstrap4
+          keyField="id"
+          columns={columns}
+          data={data}
+          defaultSorted={[
+            { dataField: 'author', order: 'asc' }
+          ]}
+          defaultSortDirection="asc"
+        />
       </section>
     </div>
   );
--- src/Readings.tsx
+++ src/Readings.tsx
@@ -1,10 +1,55 @@
 import {useContext} from 'react';
 import {Helmet} from 'react-helmet';
 import {Link} from 'react-router-dom';
+import BootstrapTable from 'react-bootstrap-table-next';
+import ToolkitProvider from 'react-bootstrap-table2-toolkit';
 import {NovelsContext} from './context';
+import { Reading } from './types';
+
+function formatTitle (_: string, reading: Reading) {
+  const {title, id} = reading;
+  return (
+    <Link className="text-lg" to={id}>{title}</Link>
+  );
+}
+
+function formatNliId (_: string, reading: Reading) {
+  const {nliId} = reading;
+  return (
+    <Link to={`/novels/${nliId}`}>{nliId}</Link>
+  );
+}
 
 export default function Readings () {
   const {readings} = useContext(NovelsContext);
+
+  const data = readings.map((n: Reading) => {
+    return {...n};
+  });
+
+  const columns = [{
+    dataField: 'title',
+    text: 'Title',
+    formatter: formatTitle,
+    sort: true,
+  }, {
+    dataField: 'author',
+    text: 'Author',
+    sort: true,
+  }, {
+    dataField: 'publisher',
+    text: 'Publisher',
+    sort: true,
+  }, {
+    dataField: 'publicationYear',
+    text: 'Year',
+    sort: true,
+  }, {
+    dataField: 'nliId',
+    text: 'NLI ID',
+    formatter: formatNliId,
+    sort: false,
+  }];
 
   return (
     <div>
@@ -13,19 +58,17 @@ export default function Readings () {
       </Helmet>
       <section>
         <h1>Readings</h1>
-        <ol dir="rtl">
-          {readings.map(reading => (
-            <li key={reading.id}>
-              <Link to={reading.id}>{reading.title}</Link>
-              {reading.nliId && (
-                <>
-                  {' ('}
-                  <Link to={`/novels/${reading.nliId}`}>{reading.nliId}</Link>)
-                </>
-              )}
-            </li>
-          ))}
-        </ol>
+
+        <BootstrapTable
+          bootstrap4
+          keyField="id"
+          columns={columns}
+          data={data}
+          defaultSorted={[
+            { dataField: 'title', order: 'asc' }
+          ]}
+          defaultSortDirection="asc"
+        />
       </section>
     </div>
   );
